f489300b15738c6b514d17009cc3c47bf5012175
frhnbaig <farhanbaig753@gmail.com>
2024-09-16 20:34:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random 
 
-def play(num_cards):
+def play(num_cards, turns_per_round):
     cards = [
         2,2,2,2,
         3,3,3,3,
@@ -22,16 +22,21 @@ def play(num_cards):
     market = select_initial_market(num_cards, cards)
     spread = select_spread()
     curr_card = 0
+    curr_sum = 0
+    expected_value_per_turn = random_noise(7.5)
+    expected_target = expected_value_per_turn * num_cards
     
-    for i in range(num_cards * 3):
-        incoming_trade = computer_play()
+    for i in range(num_cards * turns_per_round):
+        incoming_trade = computer_play(expected_target, spread)
 
         update_position(spread, incoming_trade)
         prompt_shift(spread)
 
-        if (i+1) % 3 == 0:
+        if (i+1) % turns_per_round == 0:
             curr_card += 1
+            curr_sum += market[(i+1) % turns_per_round];
             reveal_card(curr_card, market)
+            expected_target += update_expected_target(curr_sum, expected_value_per_turn, market, (i+1) % turns_per_round)
 
     show_PnL(market)
 
@@ -90,10 +95,45 @@ def shift_market(spread, direction):
         spread[1] -= 1
         spread[0] -= 1
 
-def computer_play():
+def random_noise(num):
+    scalar = random.randrange(0,2)
+    return num * scalar
+
+def update_expected_target(curr_sum, expected_value_per_turn, market, index):
+    if curr_sum < expected_value_per_turn * index:
+        return -1 * (expected_value_per_turn - market[index])
+    else:
+        return expected_value_per_turn + market[index]
+
+
+def computer_play(expected_target, spread):
     # Currently will play random move and place order for one contract until
     # I implement the actual game logic
-    move_choice = random.choice(["B", "S"])
+
+    # Maybe make a player/bot class to do all this more efficiently
+    
+    # LOGIC:
+    # Keep track of current sum
+    # Keep track of expected ending target (this is intialized at expected value of turn * num_cards) w some noise added
+    # Update current sum on reveal card call
+    # Change expected ending target, and buy and sell accordingly
+    # If expected value is above/close to buy, then buy, otherwise sell if close to/below sell (close to is defined by some arbitrary threshold determined after testing)
+    # If in the middle of range add heavier noise
+    # OR
+    # Try to minimize market maker PnL by playing sell/buy to decrease market maker PnL as much as possible
+    # Essentially hedge your previous trades, so keep track of previous trades (not implemented)
+
+    move_choice = ""
+
+    threshold = 0.5
+
+    if expected_target < spread[1] - random_noise(threshold):
+        move_choice = "S"
+    elif expected_target > spread[0] + random_noise(threshold):
+        move_choice = "B"
+    else:
+        move_choice = random.choice(["B", "S"])
+    
     move_message = ""
     if move_choice == "B":
         move_message = "Bought "
@@ -113,4 +153,4 @@ def show_PnL(market):
     print("Your total PnL was: " + str(PnL))
 
 
-play(3)
+play(3, 2)
